3401f9d063305462eff3858faec38ce6c7d85f97
GaboMISC <202020857@tese.edu.mx>
2022-08-20 18:45:49 -0500

    Versión inicial del proyecto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace WindowsService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ Temporizador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace WindowsService
+{
+    public class Temporizador
+    {
+        private readonly Timer disparador;
+        public Temporizador(int tiempo)
+        {
+            disparador = new Timer(2000);
+            disparador.Elapsed += EventoElapsed;
+        }
+
+        public void IniciarMonitoreo()
+        {
+            disparador.Start();
+        }
+
+        public void DetenerMonitoreo()
+        {
+            disparador.Stop();
+        }
+
+        private void EventoElapsed(object sender, ElapsedEventArgs e)
+        {
+            EscribirLog($"El Servicio Windows esta funcionando: {DateTime.Now}");
+        }
+
+        private void EscribirLog(string Message)
+        {
+            object objeto = new object();
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Logs";
+            string filepath = Path.Combine(path, @"ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            else
+            {
+                lock (objeto)
+                {
+                    using (StreamWriter sw = File.AppendText(filepath))
+                    {
+                        sw.WriteLine(Message);
+                    }
+                }                
+            }
+        }
+    }
+}
